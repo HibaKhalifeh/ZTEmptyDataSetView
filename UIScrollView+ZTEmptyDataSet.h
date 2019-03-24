1d6204f7d102c0731d36c80ce968298711832884
HibaKhalifeh <45962402+HibaKhalifeh@users.noreply.github.com>
2019-03-24 12:58:46 +0300

    Update UIScrollView+ZTEmptyDataSet.h

--- UIScrollView+ZTEmptyDataSet.h
+++ UIScrollView+ZTEmptyDataSet.h
@@ -3,7 +3,7 @@
 //  MedSolace
 //
 //  Created by Hiba Khalifah on 3/11/19.
-//  Copyright © 2019  ZetaTech Solutions. All rights reserved.
+//  Copyright © 2019 ZetaTech Solutions All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
@@ -20,15 +20,25 @@ NS_ASSUME_NONNULL_BEGIN
 - (NSAttributedString *)descriptionForEmptyDataSet:(UIScrollView *)scrollView;
 - (UIColor *)backgroundColorForEmptyDataSet:(UIScrollView *)scrollView;
 - (CGFloat)verticalOffsetForEmptyDataSet:(UIScrollView *)scrollView;
+- (BOOL)emptyDataSet:(UIScrollView *)scrollView enableEmptyDataSetButton:(UIButton *)button;
+
+@end
+
+@protocol ZTEmptyDataSetDelegate <NSObject>
+
+@optional
+
+- (void)emptyDataSet:(UIScrollView *)scrollView didTapButton:(UIButton *)button;
+- (UIImage *)buttonImageForEmptyDataSet:(UIScrollView *)scrollView forState:(UIControlState)state;
 
 @end
 
 @interface UIScrollView (ZTEmptyDataSet)
 
-@property (nonatomic, nullable) id <ZTEmptyDataSetSource> emptyDataSetSource;
+@property (nonatomic, nullable, weak) IBOutlet id <ZTEmptyDataSetSource> emptyDataSetSource;
+@property (nonatomic, nullable, weak) IBOutlet id <ZTEmptyDataSetDelegate> emptyDataSetDelegate;
 
 - (void)reloadEmptyDataSet;
 
 @end
-
 NS_ASSUME_NONNULL_END
